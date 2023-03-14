1155d9fcab2e0f3bac15e9fe6f084dba6ebfb6f2
silasmas <silasjmas@gmail.com>
2023-03-14 14:13:16 +0100

    ok

--- app/Http/Controllers/ReunionController.php
+++ app/Http/Controllers/ReunionController.php
@@ -74,6 +74,7 @@ class ReunionController extends Controller
         if ($retour) {
             $participant=participan::find($id[0]);
             $reunion=reunion::find($id[1]);
+            dd($participant);
             return view("pages/scanne",compact("participant","reunion"));
         } else {
 
