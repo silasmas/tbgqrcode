af2ec43aaa3f05c7c10a3b8a60371fe112b632c9
silasmas <silasjmas@gmail.com>
2023-03-14 15:10:21 +0100

    ffkf

--- resources/views/layouts/adminTemplate.blade.php
+++ resources/views/layouts/adminTemplate.blade.php
@@ -41,12 +41,11 @@
                                 <img alt="image" class="img-circle" src="{{ asset('assets/img/default.png') }}"
                                     width="100" />
                             </span>
-                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                                {{-- <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">
-                                    {{
-                                            isse(Auth::user()->prenom)?Auth::user()->prenom:"" . '-' .isset( Auth::user()->name)? Auth::user()->name:"" }}</strong> --}}
+                            {{-- <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                                <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">
+                                    {{isse(Auth::user()->prenom)?Auth::user()->prenom:"" . '-' .isset( Auth::user()->name)? Auth::user()->name:"" }}</strong>
                                     </span> <span class="text-muted text-xs block">{{ Auth::user()->fonction }} <b
-                                            class="caret"></b></span> </span> </a>
+                                            class="caret"></b></span> </span> </a> --}}
                             <ul class="dropdown-menu animated fadeInRight m-t-xs">
                                 <li><a href="">Profile</a></li>
                                 <li class="divider"></li>
