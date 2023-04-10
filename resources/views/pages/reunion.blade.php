cc650d5d072ce95799a34464ce9349f08488039c
silasmas <silasjmas@gmail.com>
2023-04-10 19:26:03 +0100

    dcsd

--- resources/views/pages/reunion.blade.php
+++ resources/views/pages/reunion.blade.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" type="text/css" href="{{asset('assets/js/parsley/parsley.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/select2/select2.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap-markdown/bootstrap-markdown.min.css') }}">
-
+<link href="{{ asset('assets/css/dataTables/datatables.min.cs') }}" rel="stylesheet">
 @endsection
 
 @section('content')
@@ -191,42 +191,59 @@
                         </div>
                     </div>
                     <div class="tab-pane" id="tab-participants">
-                        <div class="panel-body">
-                            <div class="ibox-title">
-                                <h5>Cette page affiche toutes les réunions future </h5>
-                            </div>
-                            <div class="ibox-content">
-                                <div class='row'>
-                                    <div class=" col-lg-12 col-sm-12">
-                                        <div class="full-height-scroll">
-                                            <div class="table-responsive">
-                                                <table class="table table-striped table-hover">
-                                                    <tbody>
-                                                        @forelse ($participan as $t)
-                                                        <tr>
-                                                        <td><a data-toggle="tab" href="#"
-                                                                class="client-link">{{ $t->nom." ".$t->prenom." ".$t->postnom }}</a></td>
-                                                        <td> {{ $t->sexe }}</td>
-                                                        <td class="contact-type"><i class="fa fa-clock"> </i>
-                                                        </td>
-                                                        <td>{{ $t->phone }}</td>
-                                                        <td>{{ $t->email }}</td>
-                                                        <td><span class="label label-info">Réunion(s) : {{ $t->reunion->count() }}</span></td>
-                                                        {{-- <td class="client-status text-center">
-                                                            <span class="label {{ $t->date_fin> now()?"label-primary":"label-danger" }}">Date {{ $t->date_fin> now()?$t->date_debut :"La réunion est déjà passée"}}</span>
-                                                        </td> --}}
-                                                        <td class="client-status text-center">
-                                                        <a href="{{ route('viewListeReunion',['id'=>$t->id]) }}"
-                                                        class="btn btn-xs btn-outline btn-primary">Liste des réunions participée</a>
-                                                        </td>
-
+                        <div class="ibox-title">
+                            <h5>Cette page affiche les participants de toutes les réunions </h5>
+                        </div>
+                        <div class="ibox-content">
+                            <div class='row'>
+                                <div class=" col-lg-12 col-sm-12">
+                                    <div class="full-height-scroll">
+                                        <div class="table-responsive">
+                                            <table class="table table-striped table-hover dataTables-example">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Nom Prénom Postnom</th>
+                                                        <th>Sexe </th>
+                                                        <th>Téléphone</th>
+                                                        <th>Email</th>
+                                                        <th>Nbre des réunions</th>
+                                                        <th>Option</th>
                                                     </tr>
-                                                  @empty
-                                                       <span class="label label-danger">Aucune reunion en vue</span>
-                                                  @endforelse
-                                                    </tbody>
-                                                </table>
-                                            </div>
+                                                </thead>
+                                                <tbody>
+                                                    @forelse ($participan as $t)
+                                                    <tr>
+                                                    <td><a data-toggle="tab" href="#"
+                                                            class="client-link">{{ $t->nom." ".$t->prenom." ".$t->postnom }}</a></td>
+                                                    <td> {{ $t->sexe }}</td>
+                                                   
+                                                    <td>{{ $t->phone }}</td>
+                                                    <td>{{ $t->email }}</td>
+                                                    <td><span class="label label-info">Réunion(s) : {{ $t->reunion->count() }}</span></td>
+                                                    {{-- <td class="client-status text-center">
+                                                        <span class="label {{ $t->date_fin> now()?"label-primary":"label-danger" }}">Date {{ $t->date_fin> now()?$t->date_debut :"La réunion est déjà passée"}}</span>
+                                                    </td> --}}
+                                                    <td class="client-status text-center">
+                                                    <a href="{{ route('viewListeReunion',['id'=>$t->id]) }}"
+                                                    class="btn btn-xs btn-outline btn-primary">Détail sur réunions participée</a>
+                                                    </td>
+
+                                                </tr>
+                                              @empty
+                                                   <span class="label label-danger">Aucune reunion en vue</span>
+                                              @endforelse
+                                                </tbody>
+                                                <tfoot>
+                                                    <tr>
+                                                        <th>Nom Prénom Postnom</th>
+                                                        <th>Sexe </th>
+                                                        <th>Téléphone</th>
+                                                        <th>Email</th>
+                                                        <th>Nbre des réunions</th>
+                                                        <th>Option</th>
+                                                    </tr>
+                                                </tfoot>
+                                            </table>
                                         </div>
                                     </div>
                                 </div>
@@ -252,8 +269,65 @@
 
     <script src="{{ asset('assets/js/toastr/toastr.min.js') }}"></script>
     <script src="{{ asset('assets/js/select2/select2.full.min.js') }}"></script>
+    <script src="{{ asset('assets/js/dataTables/datatables.min.js') }}"></script>
     <script>
+
         $(document).ready(function () {
+            $('.dataTables-example').DataTable({
+                language: {
+                    processing: "Traitement en cours...",
+                    search: "Rechercher&nbsp;:",
+                    lengthMenu: "Afficher _MENU_ &eacute;l&eacute;ments",
+                    info: "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                    infoEmpty: "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+                    infoFiltered: "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                    infoPostFix: "",
+                    loadingRecords: "Chargement en cours...",
+                    zeroRecords: "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                    emptyTable: "Aucune donnée disponible dans le tableau",
+                    paginate: {
+                        first: "Premier",
+                        previous: "Pr&eacute;c&eacute;dent",
+                        next: "Suivant",
+                        last: "Dernier"
+                    },
+                    aria: {
+                        sortAscending: ": activer pour trier la colonne par ordre croissant",
+                        sortDescending: ": activer pour trier la colonne par ordre décroissant"
+                    }
+                },
+                pageLength: 25,
+                responsive: true,
+                dom: '<"html5buttons"B>lTfgitp',
+                buttons: [{
+                        extend: 'copy'
+                    },
+                    {
+                        extend: 'csv'
+                    },
+                    {
+                        extend: 'excel',
+                        title: 'NewsLetter'
+                    },
+                    {
+                        extend: 'pdf',
+                        title: 'NewsLetter'
+                    },
+
+                    {
+                        extend: 'print',
+                        customize: function(win) {
+                            $(win.document.body).addClass('white-bg');
+                            $(win.document.body).css('font-size', '10px');
+
+                            $(win.document.body).find('table')
+                                .addClass('compact')
+                                .css('font-size', 'inherit');
+                        }
+                    }
+                ]
+
+            });
         $('.summernote').summernote();
 
         $("#formRubrique").on("submit", function (e) {
