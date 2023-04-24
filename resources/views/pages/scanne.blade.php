76e09873593a70b2deec05326a72094e30bbc08c
silasmas <silasjmas@gmail.com>
2023-04-24 16:08:28 +0100

    rrr

--- resources/views/pages/scanne.blade.php
+++ resources/views/pages/scanne.blade.php
@@ -11,17 +11,17 @@
 @endsection
 
 @section('content')
-<div class="wrapper wrapper-content animated fadeIn">
+{{-- <div class="wrapper wrapper-content animated fadeIn"> --}}
             <div class="middle-box text-center animated fadeInDown">
                 <h1>500</h1>
                 <h3 class="font-bold">Internal Server Error</h3>
-
                 <div class="error-desc">
                     The server encountered something unexpected that didn't allow it to complete the request. We apologize.<br/>
-                    You can go back to main page: <br/><a href="index.html" class="btn btn-primary m-t">Dashboard</a>
+                    You can go back to main page: <br/>
+                    <a href="index.html" class="btn btn-primary m-t">Dashboard</a>
                 </div>
             </div>
-</div>
+{{-- </div> --}}
     @endsection
     @section('autres-script')
     <script src="{{ asset('assets/js/bootstrap-markdown/bootstrap-markdown.js') }}"></script>
